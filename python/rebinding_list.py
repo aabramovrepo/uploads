8bc3dafb60cbc63ee517d28fe043ee2cc137bfb0
Alexey Abramov <alexey.abramov.salzi@gmail.com>
2020-09-08 17:42:15 +0200

    M:

--- python/rebinding_list.py
+++ python/rebinding_list.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def change_list(lst: List[int]) -> List[int]:
-    print(f'change_list: {lst}')  # prints [1, 2, 3]
+    print(f'change_list: lst = {lst}')  # prints [1, 2, 3]
     return lst * 2
 
 
@@ -12,6 +12,22 @@ def run(source: List[int]) -> List[int]:
     print(f'run: source = {source}')  # prints [1, 2, 3, 1, 2, 3]
     ret = source + [4, 5]
     print(f'run: ret = {ret}')  # prints [1, 2, 3, 1, 2, 3, 4, 5]
+
+    return ret
+
+
+def change_list_mutability(lst: List[int]) -> List[int]:
+    print(f'change_list_mutability: lst = {lst}')  # prints [1, 2, 3]
+    lst *= 2
+    return lst
+
+
+def run_mutability(source: List[int]) -> List[int]:
+    print(f'run_mutability: source = {source}')  # prints [1, 2, 3]
+    change_list_mutability(source)  # assignment not needed!!!
+    print(f'run_mutability: source = {source}')  # [1, 2, 3, 1, 2, 3]
+    ret = source + [4, 5]
+    print(f'run_mutability: ret = {ret}')  # prints [1, 2, 3, 1, 2, 3, 4, 5]
 
     return ret
 
@@ -28,8 +44,18 @@ def main() -> None:
 
     res = run(lst)
 
-    print(f'main: res = {res}')  # prints [1, 2, 3, 1, 2, 3, 4]
+    print(f'main: res = {res}')  # prints [1, 2, 3, 1, 2, 3, 4, 5]
     print(f'main: lst = {lst}')  # prints [1, 2, 3]
+
+    print('\n\n')
+
+    # here we will make use of mutability
+    # WARNING: Python libs (e.g. OpenCV) use rebinding
+    # and do not mutate input parameters!
+    res = run_mutability(lst)
+
+    print(f'main: res = {res}')  # prints [1, 2, 3, 1, 2, 3, 4, 5]
+    print(f'main: lst = {lst}')  # prints [1, 2, 3, 1, 2, 3]
 
 
 if __name__ == '__main__':
